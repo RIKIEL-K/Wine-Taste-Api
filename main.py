587e3c3cbdbf644873c6e1be4a2053483247b05e
RIKIEL-K <rikkouambeng05@gmail.com>
2025-02-13 15:20:08 -0500

    add the breakpoints to get the preferred vines on the files

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
 import pandas as pd
-from flask import Flask
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/api/<pays>")
+def vine_region(pays):
+    df = pd.read_csv("wines.csv")
+    df1= df[["country","name","price","province","points"]].loc[(df["country"]==pays) & (df["points"] == 100)]
+    result = df1.to_dict(orient="records")
+    return result
+
+
+if __name__ == "__main__":
+    app.run(debug=True,port=5000)
